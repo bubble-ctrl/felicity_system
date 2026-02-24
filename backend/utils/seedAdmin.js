e9b245900313ce5d5e82eaf5a87c18064a5750be
Laveena Jain <laveena.jain@research.iiit.ac.in>
2026-02-24 18:32:09 +0530

    Fix seedAdmin path resolution

--- backend/utils/seedAdmin.js
+++ backend/utils/seedAdmin.js
@@ -1,5 +1,7 @@
-const User = require('../models/User');
-const { ROLES } = require('../config/constants');
+import { join } from 'path';
+
+const User = require(join(__dirname, '../models/User'));
+const { ROLES } = require(path.join(__dirname, '../config/constants'));
 
 const ADMIN_EMAIL = process.env.ADMIN_EMAIL || 'admin@felicity.iiit.ac.in';
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'Admin@123';
@@ -28,4 +30,4 @@ const seedAdmin = async () => {
     }
 };
 
-module.exports = seedAdmin;
+export default seedAdmin;
